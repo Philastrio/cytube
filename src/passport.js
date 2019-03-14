f910222d369a3db92caa43ce85741cf4b9e80976
philastrio <han_gang@naer.com>
2019-03-14 22:43:04 +0900

    Add heroku site

--- src/passport.js
+++ src/passport.js
@@ -17,7 +17,9 @@ passport.use(
     {
       clientID: process.env.GH_ID,
       clientSecret: process.env.GH_SECRET,
-      callbackURL: `http://localhost:4000${routes.githubCallback}`
+      callbackURL: `https://young-shelf-30020.herokuapp.com${
+        routes.githubCallback
+      }`
     },
     githubLoginCallback
   )
