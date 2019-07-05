1455e14958736fa3125af4cdaf7f8378c963cb5f
HanKang <han_gang@naver.com>
2019-07-06 05:07:12 +0900

    aws 수정

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,35 +1,35 @@
-import multer from 'multer';
-import multerS3 from 'multer-s3';
-import aws from 'aws-sdk';
-import routes from './routes';
+import multer from "multer";
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
+import routes from "./routes";
 
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_PRIVATE_KEY,
-  region: 'ap-northeast-2'
+  region: "ap-northeast-2"
 });
 
 const multerVideo = multer({
   storage: multerS3({
     s3,
-    acl: 'public-read',
-    bucket: 'chtube/video'
+    acl: "public-read",
+    bucket: "cytube/video"
   })
 });
 
 const multerAvatar = multer({
   storage: multerS3({
     s3,
-    acl: 'public-read',
-    bucket: 'chtube/avatars'
+    acl: "public-read",
+    bucket: "cytube/avatars"
   })
 });
 
-export const uploadVideo = multerVideo.single('videoFile');
-export const uploadAvatar = multerAvatar.single('avatar');
+export const uploadVideo = multerVideo.single("videoFile");
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddlewares = (req, res, next) => {
-  res.locals.siteName = '청양Tube';
+  res.locals.siteName = "청양Tube";
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null;
   next();
